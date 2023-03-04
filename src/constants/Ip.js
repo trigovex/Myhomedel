efec54b919c7566b58f06ee6b0118d81f36bb552
trigovex <trigovextechnologies@gmail.com>
2023-03-04 23:26:20 +0530

    first com

--- src/constants/Ip.js
+++ src/constants/Ip.js
@@ -1,4 +1,4 @@
-export const Ip = "https://vast-ruby-duck-wrap.cyclic.app";
+export const Ip = "https://wild-red-beetle-gear.cyclic.app";
 
 //https://myhomedel.herokuapp.com
 //http://192.168.55.104:5000
