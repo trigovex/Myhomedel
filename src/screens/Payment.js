688e5a2464263b9d5b9e0ddac37942d619e779dc
trigovex <trigovextechnologies@gmail.com>
2023-03-07 00:31:16 +0530

    first com

--- src/screens/Payment.js
+++ src/screens/Payment.js
@@ -220,10 +220,8 @@ function Payment(props) {
 
       var val = Math.floor(1000 + Math.random() * 9000);
 
-      var tax= (sum/100)*5;
-     
-
-   
+      var taxvalue= (sum/100)*5;
+      var tax =num.toFixed(2);
  
 
       const [Field,setField] =useState(false);
